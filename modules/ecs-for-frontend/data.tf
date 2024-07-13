375e23f9c0d9859ed7edba92bdd11013d04963a5
Joshua Zimmerman <jpz6926@gmail.com>
2024-07-12 22:19:03 -0400

    adding base code for barnone frontend ecs task

---
+++ modules/ecs-for-frontend/data.tf
@@ -0,0 +1,14 @@
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy" "ecs_task_execution_role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
