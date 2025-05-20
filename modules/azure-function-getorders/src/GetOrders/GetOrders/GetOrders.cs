392f0ec5dd97e334e640d1b02ee43d43d9175f03
Joshua Zimmerman <jpz6926@gmail.com>
2025-05-20 02:32:28 -0400

    Maintenance, code cleanup: updating function .net version from .NET6 to .NET8, removing vs files from source control, cleaning up function code

--- modules/azure-function-getorders/src/GetOrders/GetOrders/GetOrders.cs
+++ modules/azure-function-getorders/src/GetOrders/GetOrders/GetOrders.cs
@@ -3,11 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Data;
-using System.IO;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -17,15 +15,8 @@ namespace GetOrders
     {
         [FunctionName("GetOrders")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
         {
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-
             try
             {
                 using (var connection = new SqlConnection("DB_CONNECTION"))
